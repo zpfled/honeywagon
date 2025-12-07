0fc23f42dea1803fe673a8103962d2438a2a1113
Zach Pflederer <zpfled@gmail.com>
2025-12-07 09:18:34 -0600

    set up unit types

--- app/models/unit_type.rb
+++ app/models/unit_type.rb
@@ -1,5 +1,11 @@
 class UnitType < ApplicationRecord
   has_many :units, dependent: :nullify
 
+  TYPES = [
+    { name: "Standard Unit", slug: "standard" },
+    { name: "ADA Accessible Unit", slug: "ada" },
+    { name: "Handwash Station", slug: "handwash" }
+  ].freeze
+
   def to_s = name
 end
