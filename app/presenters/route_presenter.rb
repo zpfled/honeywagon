2373c6387875443bf6b25072e7881d38cba84c2a
Zach Pflederer <zpfled@gmail.com>
2025-12-27 14:13:04 -0600

    implement RoutePresenter

--- app/presenters/route_presenter.rb
+++ app/presenters/route_presenter.rb
@@ -1,10 +1,28 @@
-# frozen_string_literal: true
-
-# TODO: RoutePresenter should encapsulate route-level display data
-# (summary stats, warnings, drive estimates) for views/partials.
 class RoutePresenter
   def initialize(route)
     @route = route
+  end
+
+  def humanized_drive_time
+    seconds = route.estimated_drive_seconds.to_i
+    return nil unless seconds.positive?
+
+    hours = seconds / 3600
+    minutes = (seconds % 3600) / 60
+
+    if hours.positive?
+      "#{hours}h #{minutes}m"
+    else
+      "#{minutes}m"
+    end
+  end
+
+  def humanized_drive_distance
+    meters = route.estimated_drive_meters.to_f
+    return nil unless meters.positive?
+
+    miles = meters / 1609.34
+    "#{miles.round(1)} mi"
   end
 
   private
--- spec/presenters/route_presenter_spec.rb
+++ spec/presenters/route_presenter_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
-# TODO: Flesh out RoutePresenter specs once formatting responsibilities are defined.
 RSpec.describe RoutePresenter do
-  it 'is initialized with a route' do
-    route = build_stubbed(:route)
-    presenter = described_class.new(route)
-    expect(presenter).to be_present
+  let(:route) { build_stubbed(:route, estimated_drive_seconds: 8100, estimated_drive_meters: 1609.34) }
+  let(:presenter) { described_class.new(route) }
+
+  describe '#humanized_drive_time' do
+    it 'returns formatted time (happy path)' do
+      expect(presenter.humanized_drive_time).to eq('2h 15m')
+    end
+
+    it 'returns nil when no estimate (sad/bad path)' do
+      allow(route).to receive(:estimated_drive_seconds).and_return(0)
+      expect(presenter.humanized_drive_time).to be_nil
+    end
+  end
+
+  describe '#humanized_drive_distance' do
+    it 'returns formatted distance (happy path)' do
+      expect(presenter.humanized_drive_distance).to eq('1.0 mi')
+    end
+
+    it 'returns nil when no estimate (sad/bad path)' do
+      allow(route).to receive(:estimated_drive_meters).and_return(0)
+      expect(presenter.humanized_drive_distance).to be_nil
+    end
   end
 end
